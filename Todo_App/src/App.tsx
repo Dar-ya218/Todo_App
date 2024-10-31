676f654858e4268b6fcb0c25be741d062591f299
Dasha <dasharoura13465@gmail.com>
2024-10-31 22:53:55 +0100

    add vite.config

--- Todo_App/src/App.tsx
+++ Todo_App/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import { Toaster, toast } from 'sonner';
 import { Navbar } from './components/Navbar';
